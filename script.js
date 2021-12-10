1afc7d825176fb35fc789f456a8fffb049b67a89
chinsan <clim1994@gmail.com>
2021-12-10 13:22:18 -0800

    project is in MVP

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('meow meow');
+// --------------------------------- DECK CREATION --------------------------------- //
 
 // CREATE 52 card deck
 
@@ -8,7 +8,7 @@ const Cards = function (suit, rank, score) {
 	this.score = score;
 };
 
-const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
+const suits = ['♠︎', '♥︎', '♣︎', '♦︎'];
 const ranks = [
 	'A',
 	'2',
@@ -35,12 +35,29 @@ for (let i = 0; i < suits.length; i++) {
 
 console.log(FiftyTwoCards);
 
+// INITIALIZE J, Q, K cards with value of '10'
+
+for (let i = 0; i < FiftyTwoCards.length; i++) {
+	if (
+		FiftyTwoCards[i].rank === 'J' ||
+		FiftyTwoCards[i].rank === 'Q' ||
+		FiftyTwoCards[i].rank === 'K'
+	) {
+		FiftyTwoCards[i].score = 10;
+	} else if (FiftyTwoCards[i].rank === 'A') {
+		FiftyTwoCards[i].score = 11;
+	}
+}
+
 //CREATE 8 DECKS
 
+// ---------------------------------- VARIABLES ---------------------------------- //
 // DECLARE constants for deck, blackjack
 
 const deck = FiftyTwoCards;
 const blackjack = 21;
+const playerHandArr = [];
+const dealerHandArr = [];
 
 // DECLARE state variables for bankroll, betAmt, deckSize
 
@@ -51,10 +68,14 @@ let cardTotal;
 
 // DECLARE object for player/dealer with cardTotal
 
-let player;
-let dealer;
+let playerSum = 0;
+let dealerSum = 0;
+
+const sum = (previousCard, currentCard) => previousCard + currentCard;
 
 // REFERENCE hitBtn, standBtn, startBtn, howToPlayBtn, startModalBtn, closeHowToPlayBtn, reDealBtn, card divs for dealer and player
+
+// ---------------------------------- DOM ELEMENTS ---------------------------------- //
 
 const hitBtn = document.querySelector('.hit');
 const dblBtn = document.querySelector('.double');
@@ -62,31 +83,36 @@ const standBtn = document.querySelector('.stand');
 const startBtn = document.querySelector('.start-game');
 const reDealBtn = document.querySelector('.redeal');
 
+let playerHand = document.querySelector('.player-container');
+let dealerHand = document.querySelector('.dealer-container');
+
 let playerBot = document.querySelector('.player-bottom-card');
 let playerTop = document.querySelector('.dealer-top-card');
 
 let dealerBot = document.querySelector('.dealer-bottom-card');
 let dealerTop = document.querySelector('.dealer-top-card');
 
-// make sure html classes match these event listeners
-// const howToPlayBtn = document.querySelector('.how-to-play');
-// const startModalBtn = document.querySelector('.start-from-modal');
-// const closeHowToPlayBtn = document.querySelector('.close-from-htp');
+let winner = document.querySelector('.winner');
 
-// INITIALIZE J, Q, K cards with value of '10'
-// INITIALIZE ACE cards with value of '1' IF cardTotal <= 17 or '11' IF cardTotal > 17
+// make sure html classes match these event listeners
+const howToPlayBtn = document.querySelector('.how-to-play');
+const startModalBtn = document.querySelector('.start-from-modal');
+const closeHowToPlayBtn = document.querySelector('.close-from-htp');
 
 // ATTACH event listener to: - START - HOWTOPLAY - HIT - STAND - DOUBLE - SPLIT - REDEAL
 
-hitBtn.addEventListener('click', addCard);
-dblBtn.addEventListener('click', dblBet);
+// ------------------------------ EVENT LISTENERS ------------------------------ //
+
+hitBtn.addEventListener('click', playerHit);
+// dblBtn.addEventListener('click', dblBet);
 standBtn.addEventListener('click', stand);
 startBtn.addEventListener('click', dealCards);
-reDealBtn.addEventListener('click', redealCards);
-// DECLARE function to:
+// reDealBtn.addEventListener('click', startGame);
 
-// - DEAL
-//   randomly assign card to player (face up), dealer (face up), player (face up), dealer (face down)
+// ---------------------------------- FUNCTIONS ---------------------------------- //
+
+// SHUFFLE THE DECK
+// bubblesorting cards in random locations
 
 function shuffleCards() {
 	for (let i = 0; i < FiftyTwoCards.length; i++) {
@@ -97,65 +123,139 @@ function shuffleCards() {
 	}
 }
 
-shuffleCards(); // need logic for "card cut" when user selects number of decks and cut
-//!! is there a better way to do this
-//!! would it make more sense to put hands into an array?
+shuffleCards();
+
+// DEAL OUT 4 CARDS
+// alternate the deal, starting with player and end with dealer
+// remove dealt cards from the original deck
+
 function dealCards() {
 	playerBot = FiftyTwoCards[0];
+	playerHandArr.push(FiftyTwoCards[0].score);
 	document.getElementById('player-bottom-card').innerHTML =
 		FiftyTwoCards[0].rank;
 	FiftyTwoCards.shift();
+	//
 	document.getElementById('dealer-bottom-card').innerHTML =
 		FiftyTwoCards[1].rank;
+	dealerHandArr.push(FiftyTwoCards[1].score);
 	FiftyTwoCards.shift();
+	//
 	playerTop = FiftyTwoCards[2];
+	playerHandArr.push(FiftyTwoCards[2].score);
 	document.getElementById('player-top-card').innerHTML = FiftyTwoCards[2].rank;
 	FiftyTwoCards.shift();
+	//
 	dealerTop = FiftyTwoCards[3];
 	document.getElementById('dealer-top-card').innerHTML = FiftyTwoCards[3].rank;
+	dealerHandArr.push(FiftyTwoCards[3].score);
 	FiftyTwoCards.shift();
 }
 
-dealCards();
-console.log(FiftyTwoCards);
+// CHECK FOR BLACKJACK EACH ITERATION
 
-// - HIT
-//   add card
+// card totals determined from player/dealer HandArr, populated from deal and each time player hits, dealer adds a card
 
-function addCard() {
-	// newCardSlot = document.createElement('div');
-	// newCardSlot.appendChild(FiftyTwoCards[0]);
-	// console.log(newCardSlot);
-	// FiftyTwoCards.shift();
+function playerCardTotal() {
+	playerSum = playerHandArr.reduce(sum);
+
+	for (let i = 0; i < playerHandArr.length; i++) {
+		if (playerHandArr[i] === 11 && playerSum > 21) {
+			playerSum -= 10;
+		}
+	}
 }
 
-// - DOUBLE
-//   double bet and add one card
+function dealerCardTotal() {
+	dealerSum = dealerHandArr.reduce(sum);
 
-function dblBet() {
-	console.log('double meow');
+	for (let i = 0; i < dealerHandArr.length; i++) {
+		if (dealerHandArr[i] === 11 && dealerHandArr > 21) {
+			dealerHandArr -= 10;
+		}
+	}
 }
 
-// - STAND
-//   end player turn
+// initial: check for player/dealer blackjack
+// in-game: check for blackjack each time a card is add
 
+function checkForBlackjack() {
+	if (playerSum === 21) {
+		console.log('player blackjack');
+	} else if (playerSum > 21) {
+		console.log('player BUST');
+	}
+
+	if (dealerSum === 21) {
+		console.log('dealer blackjack');
+	} else if (dealerSum > 21) {
+		console.log('dealer BUST');
+	}
+
+	if (playerSum === dealerSum) {
+		console.log('PUSH');
+	}
+}
+
+// PLAYER CHOOSE TO HIT OR STAND
+// hit will add another card to player hand/array
+// stand will move the gameplay to the dealer hit
+
+function playerHit() {
+	//div parent: player-container
+
+	newCardSlot = document.createElement('div');
+	newCardSlot.innerHTML = FiftyTwoCards[0].rank;
+	playerHand.appendChild(newCardSlot);
+	playerHandArr.push(FiftyTwoCards[0].score);
+	FiftyTwoCards.shift();
+	playerCardTotal();
+	checkForBlackjack();
+
+	console.log(playerSum);
+}
+
+function dealerHit() {
+	while (dealerSum < 17) {
+		if (dealerSum < 17) {
+			newCardSlot = document.createElement('div');
+			newCardSlot.innerHTML = FiftyTwoCards[0].rank;
+			dealerHand.appendChild(newCardSlot);
+			dealerHandArr.push(FiftyTwoCards[0].score);
+			FiftyTwoCards.shift();
+			dealerCardTotal();
+			checkForBlackjack();
+			console.log(dealerSum);
+		}
+	}
+}
 function stand() {
-	console.log('meow stand');
+	hitBtn.disabled = true;
+	dealerHit();
 }
 
-// - REDEAL
-//   round has ended, alter bankroll, remove played cards from divs/deck and redeal new cards
+function compareHands() {
+	playerCardTotal();
+	dealerCardTotal();
 
-function redealCards() {
-	console.log('redeal right meow');
+	//instances: win, lose, push
 }
 
-// - DEALER HIT
-//   add cards until dealer.cardTotal > 17
-// - COMPARE
-//   player.cardTotal and dealer.cardTotal
+// ---------------------------------- START GAME ---------------------------------- //
 
-// SUBTRACT betAmt from bankroll IF player.cardTotal > 21 OR dealer.cardTotal > player.cardTotal
-// ADD betAmt to bankroll IF dealer.cardTotal > 21 OR dealer.cardTotal < player.cardTotal
+function startGame() {
+	dealCards();
 
-// CLICK REDEAL btn to restart the game, with updated bankroll
+	playerCardTotal();
+	dealerCardTotal();
+
+	checkForBlackjack();
+
+	console.log(playerHandArr);
+	console.log(dealerHandArr);
+
+	console.log(dealerSum);
+	console.log(playerSum);
+}
+
+startGame();
